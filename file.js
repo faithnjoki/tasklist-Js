622b05124522a725a2e3d5f20bc229139c5844ab
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-06-21 15:12:10 +0300

    remove delete button from html file, added functionality to delete button

--- file.js
+++ file.js
@@ -1,15 +1,26 @@
 
-document.querySelector('form').addEventListener('submit',(e) => {
+ document.querySelector('form').addEventListener('submit',(e) => {
+    let form = document.querySelector('form')
     event.preventDefault();
     handleTodo(e.target.newtask.value)
+    // resets form once one adds a task
+    form.reset()
 })
 
 function handleTodo(todo){
 //   creating a new paragraph to take out tasks lists
     let p = document.createElement('p');
+    let btn = document.createElement('button');
+    btn.addEventListener('click',handledelete)
+    btn.innerHTML = ' Delete';
     p.textContent = todo
-    console.log(p)
-    document.querySelector('#taskstodo').appendChild(p)
+    p.appendChild(btn)
+    document.querySelector('#taskstodo').appendChild(p);
 
+
+}
+
+function handledelete(e){
+    e.target.parentNode.remove()
 
 }
